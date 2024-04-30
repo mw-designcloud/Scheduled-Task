b74d097c3dff200f51285b2c4e9dc484abbc4d89
Ned Gulley <gulley@gmail.com>
2024-04-30 12:07:16 -0400

    Add the URL back to the repeating task

--- Tasks/task1.m
+++ Tasks/task1.m
@@ -1,4 +1,5 @@
 tLeaderboard = readtable("Data/cody_leaders.csv");
+url = "https://www.mathworks.com/matlabcentral/cody/players?term=id%3A173294+OR+id%3A734801+OR+id%3A5349647+OR+id%3A17355128+OR+id%3A1887879+OR+id%3A17719399+OR+id%3A12862873+OR+id%3A15363629+OR+id%3A615500+OR+id%3A10491973&format=json";
 json = webread(url);
 tCurrentScores = struct2table(json);
 tNewLeaders = array2table(tCurrentScores.score');
