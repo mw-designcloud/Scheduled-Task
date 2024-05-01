fada7168cf2b8295ae93fab476e3f54c3a36683e
Ned Gulley <gulley@gmail.com>
2024-05-01 16:34:44 -0400

    Refactor and clean up task1

--- Tasks/task1.m
+++ Tasks/task1.m
@@ -1,11 +1,80 @@
-tLeaderboard = readtable("Data/cody_leaders.csv");
-url = "https://www.mathworks.com/matlabcentral/cody/players?term=id%3A173294+OR+id%3A734801+OR+id%3A5349647+OR+id%3A17355128+OR+id%3A1887879+OR+id%3A17719399+OR+id%3A12862873+OR+id%3A15363629+OR+id%3A615500+OR+id%3A10491973&format=json";
-json = webread(url);
-tCurrentScores = struct2table(json);
-tNewLeaders = array2table(tCurrentScores.score');
-names = string(tCurrentScores.nickname');
-firstNames = regexprep(names," .*","");
-tNewLeaders.Properties.VariableNames = firstNames;
-tNewLeaders.time = datetime("now");
-tLeaderboard = [tLeaderboard; tNewLeaders];
-writetable(tLeaderboard,'Data/cody_leaders.csv')
+function task1(option)
+    % option =  
+    %     "initialize" for the first pass or 
+    %     "append"     for all subsequent passes
+
+    arguments
+        option string {mustBeMember(option,["initialize","append"])} = "append"
+    end
+
+    % These are the Cody profile IDs of the people we're tracking 
+    profile_ids = [ ...
+        173294 
+        734801 
+        5349647 
+        17355128 
+        1887879 
+        17719399 
+        12862873 
+        15363629 
+        615500 
+        10491973];
+
+    url = makeUrl(profile_ids);
+
+    leaderboardFilename = "Data/cody_leaders.csv";
+
+    json = webread(url);
+    tCurrentScores = struct2table(json);
+    tLeaderboard = array2table(tCurrentScores.score');
+    names = string(tCurrentScores.nickname');
+    firstNames = regexprep(names," .*","");
+
+    tLeaderboard.Properties.VariableNames = firstNames;
+    dtNow = datetime("now");
+    tLeaderboard.time = dtNow;
+
+    if option=="initialize"
+
+        writetable(tLeaderboard,leaderboardFilename)
+
+    elseif option=="append"
+
+        tLeaderboardPrevious = readtable(leaderboardFilename);
+        tLeaderboard = [tLeaderboardPrevious; tLeaderboard];
+        writetable(tLeaderboard,leaderboardFilename)
+
+        % Print out this leaderboard plot once a week
+        dtNow.Format = "eeee";
+        if string(dtNow)=="Monday"
+            plotLeaderboard(tLeaderboard, firstNames);
+        end
+
+    end
+
+end
+
+function plotLeaderboard(tLeaderboard, firstNames)
+    
+    tt = table2timetable(tLeaderboard);
+    for n = 1:width(tt)
+        plot(tt.time,tt.(firstNames(n)))
+        hold all
+    end
+    hold off
+    yt = yticks;
+    set(gca,YTickLabel=string(yt))
+    legend(firstNames,Location="eastoutside")
+    print("Scores","-dpng")
+
+end
+
+function url = makeUrl(profile_ids)
+    % For making the RESTful call to the Cody API
+
+    s1 = "https://www.mathworks.com/matlabcentral/cody/players?term=id%3A";
+    s2 = join(string(profile_ids),"+OR+id%3A");
+    s3 = "&format=json";
+    url = s1 + s2 + s3;
+
+end
